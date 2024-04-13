7686a242c1ac8d437e3f624d7b1d59670a6d92d5
Efraim Nabil <efraimefefraim@gmail.com>
2024-04-13 17:49:21 +0200

    Add Redux Toolkit and React Redux dependencies and create a store with a slice for the auth

---
+++ src/app/features/authSlice.tsx
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../store';
+import { IDoctor } from '../../interfaces';
+import Cookies from 'js-cookie'
+
+export interface AuthState {
+  token: string | null;
+  doctor: IDoctor | null;
+}
+
+const initialState: AuthState = {
+  token: null,
+  doctor: null,
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    login: (state, action: PayloadAction<Partial<AuthState>>) => {
+      state.token = action.payload.token || state.token;
+      state.doctor = action.payload.doctor || state.doctor;
+    },
+    logout: (state) => {
+      state.token = null;
+      state.doctor = null;
+      Cookies.remove('token');
+    },
+  },
+})
+
+export const { login, logout } = authSlice.actions;
+export const selectToken = (state: RootState) => state.auth.token;
+export const selectDoctor = (state: RootState) => state.auth.doctor;
+
+export default authSlice.reducer;
