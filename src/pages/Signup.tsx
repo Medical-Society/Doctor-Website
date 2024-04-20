ded62a938b033bcfaf9d575d84164f2d3abe32f6
Efraim Nabil <efraimefefraim@gmail.com>
2024-04-20 23:09:25 +0200

    Refactor Signup and is Loading logic

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -2,14 +2,25 @@ import { useState, useEffect } from "react";
 import { ISignupErrors, ISignupState } from "../interfaces";
 import HaveAccountOrNot from "../Components/authForms/HaveAccountOrNot";
 import toast from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
 import { validateSignup } from "../validations";
 import { useRegisterMutation } from "../services/authApi";
 //import { FormInputlist } from "../data/data";
 //import FormInput from "../Components/authForms/FormInput";
-import SignupStepper from "../Components/signup/SignupStepper";
+import { FormInputlist } from "../data/data";
+import FormInput from "../Components/authForms/FormInput";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Step,
+  StepLabel,
+  Stepper,
+  Typography,
+} from "@mui/material";
 
 const Signup = () => {
+  const steps = ["Account info", "Personal info", "Clinic info"];
+
   const defaultDoctor: ISignupState = {
     englishFullName: "",
     arabicFullName: "",
@@ -24,10 +35,10 @@ const Signup = () => {
     gender: "",
   };
 
-  const navigate = useNavigate();
+  const [activeStep, setActiveStep] = useState(0);
+  const [signupData, setSignupData] = useState<ISignupState>(defaultDoctor);
   const [registerUser, { data, isSuccess, isLoading, isError, error }] =
     useRegisterMutation();
-  const [signupData, setSignupData] = useState<ISignupState>(defaultDoctor);
   const [errors, setErrors] = useState<ISignupErrors>({
     englishFullName: "",
     arabicFullName: "",
@@ -39,25 +50,6 @@ const Signup = () => {
     nationalID: "",
     phoneNumber: "",
   });
-  /*    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setSignupData({
-        ...signupData,
-        [name]: value
-    }); 
-    setErrors({
-        ...errors,
-        [name]: ''
-    });
-  };
-
-  const handleChangeSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const { name, value } = e.target;
-    setSignupData({
-        ...signupData,
-        [name]: value
-    });
-  };  */
 
   const handleSubmit = async (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
@@ -74,7 +66,6 @@ const Signup = () => {
     }
     const { confirmPassword, ...data } = signupData;
     await registerUser({ ...data });
-    console.log(signupData);
   };
 
   useEffect(() => {
@@ -83,50 +74,174 @@ const Signup = () => {
       toast.success("Signed up successfully, Please Verify your email", {
         duration: 8000,
       });
-      navigate("/login");
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
     }
     if (isError && error) {
+      console.log(error);
       const errorMessage = error as { data: { message: string } };
       toast.error(errorMessage.data.message);
     }
   }, [isSuccess, isError, data, error]);
 
-  /*   const renderFormInputList = FormInputlist.map(input => (
-    <div key={input.id}>
-      <FormInput
-        label={input.label}
-        type={input.type}
-        id={input.id}
-        name={input.name}
-        value={signupData[input.name]}
-        onChange={(e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-          if (input.type === "select") {
-            handleChangeSelect(e as React.ChangeEvent<HTMLSelectElement>);
-          } else {
-            handleChange(e as React.ChangeEvent<HTMLInputElement>);
+  const renderFormInputsForStep = (step: number) => {
+    const handleChange = (
+      e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+    ): void => {
+      const { name, value } = e.target;
+      setSignupData((prevSignup: any) => ({
+        ...prevSignup,
+        [name]: value,
+      }));
+      setErrors((prevErrors: any) => ({
+        ...prevErrors,
+        [name]: "",
+      }));
+    };
+
+    return (
+      <div className="grid grid-cols-2 grid-rows-3 gap-3">
+        {FormInputlist.filter((input) => {
+          if (step === 0) {
+            return [
+              "englishFullName",
+              "arabicFullName",
+              "email",
+              "password",
+              "confirmPassword",
+            ].includes(input.name);
+          } else if (step === 1) {
+            return [
+              "specialization",
+              "nationalID",
+              "phoneNumber",
+              "birthdate",
+              "gender",
+            ].includes(input.name);
+          } else if (step === 2) {
+            return ["clinicAddress"].includes(input.name);
           }
-        }}
-        placeholder={input.placeholder}
-        options={input.options}
-        errorMsg={errors[input.name as keyof ISignupErrors]}
-        signup
-      />
-    </div>
-)); */
+          return true;
+        }).map((input) => (
+          <div key={input.id} className="flex flex-col">
+            <div>
+              <FormInput
+                label={input.label}
+                type={input.type}
+                id={input.id}
+                name={input.name}
+                value={signupData[input.name]}
+                onChange={handleChange}
+                placeholder={input.placeholder}
+                options={input.options}
+                errorMsg={errors[input.name as keyof ISignupErrors]}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  const handleNext = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const validationErrors = validateSignup(signupData, activeStep);
+    const hasErrors = Object.values(validationErrors).some(
+      (errMsg) => errMsg !== ""
+    );
+
+    if (hasErrors) {
+      setErrors(validationErrors);
+    } else {
+      if (activeStep === steps.length - 1) {
+        console.log("submitted");
+        handleSubmit(e);
+      } else {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+      }
+    }
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
   return (
     <div className="flex justify-center items-center mt-14">
       <div className="flex flex-col justify-center items-center">
         <h1 className="text-primary text-3xl font-bold mb-4">Signup</h1>
         <div className="rounded-xl bg-gradient-to-r from-primary to-secondary p-0.5 lg:min-w-max mb-4">
           <form className="grid grid-cols-1 md:grid-cols-2 gap-2 bg-white rounded-xl py-8 px-5">
-            <SignupStepper
-              errors={errors}
-              setErrors={setErrors}
-              setSignup={setSignupData}
-              signup={signupData}
-              isLoading={isLoading}
-              handleSubmit={handleSubmit}
-            />
+            <Box
+              sx={{
+                width: { sm: "600px", md: "750px" },
+                margin: "auto",
+                marginRight: { md: "-350px", sm: "-350" },
+              }}
+            >
+              <Stepper activeStep={activeStep}>
+                {steps.map((label) => {
+                  const stepProps: { completed?: boolean } = {};
+                  const labelProps: { optional?: React.ReactNode } = {};
+                  return (
+                    <Step key={label} {...stepProps}>
+                      <StepLabel
+                        {...labelProps}
+                        StepIconProps={{ style: { color: "#060B73" } }}
+                      >
+                        {label}
+                      </StepLabel>
+                    </Step>
+                  );
+                })}
+              </Stepper>
+
+              {activeStep === steps.length ? (
+                <>
+                  <Typography component="div" sx={{ mt: 2, mb: 1 }}>
+                    <h1 className="text-primary text-2xl font-bold mb-4">
+                      All steps completed - you&apos;re finished
+                    </h1>
+                    <p className="text-gray-500 text-lg">
+                      Please verify your email
+                    </p>
+                  </Typography>
+                  <Box sx={{ display: "flex", flexDirection: "column", pt: 3 }}>
+                    <Box sx={{ flex: "1 1 auto" }} />
+                  </Box>
+                </>
+              ) : (
+                <>
+                  <Typography component="div" sx={{ mt: 2, mb: -3 }}>
+                    {renderFormInputsForStep(activeStep)}
+                  </Typography>
+                  <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                    <Button
+                      color="inherit"
+                      disabled={activeStep === 0}
+                      onClick={handleBack}
+                      sx={{ mr: 1 }}
+                    >
+                      Back
+                    </Button>
+                    <Box sx={{ flex: "1 1 auto" }} />
+                    <Button
+                      type="button"
+                      onClick={(
+                        e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+                      ) => handleNext(e)}
+                      disabled={isLoading} // Disable the button while loading
+                    >
+                      {isLoading ? (
+                        <CircularProgress size={24} /> // Show a loading spinner while loading
+                      ) : activeStep === steps.length - 1 ? (
+                        "Finish"
+                      ) : (
+                        "Next"
+                      )}
+                    </Button>
+                  </Box>
+                </>
+              )}
+            </Box>
 
             <div className="lg:mt-80 lg:top-96 lg:left-16 lg:transform lg:-translate-x-1/2 lg:-translate-y-1/2">
               {" "}
