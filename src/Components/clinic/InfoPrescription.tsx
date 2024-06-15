f04cbca90a52e0b4ae9452ee383f1cc99901b711
Efraim Nabil <efraimefefraim@gmail.com>
2024-06-15 04:00:15 +0300

    formate code

--- src/Components/clinic/InfoPrescription.tsx
+++ src/Components/clinic/InfoPrescription.tsx
@@ -3,9 +3,7 @@ interface IProps {
   patient: IPatient;
 }
 
-
-const InfoPrescription = ({patient} : IProps) => {
-  
+const InfoPrescription = ({ patient }: IProps) => {
   const birthDate = new Date(patient.birthdate);
   const ageDifMs = Date.now() - birthDate.getTime();
   const ageDate = new Date(ageDifMs);
@@ -13,27 +11,35 @@ const InfoPrescription = ({patient} : IProps) => {
 
   const handleShowMedicalHistory = () => {
     console.log("Show Medical History");
-  }
-    return ( 
-       <div className="flex flex-col gap-2 mt-5">
-         <div className="flex flex-row">
-           <div className="text-neutral-800 text-[22px] font-medium font-['Cairo'] leading-relaxed">Name :</div>
-              <span className="text-neutral-800 text-[22px] font-light font-['Cairo'] leading-relaxed md:pl-10 pl-4">{patient.patientName}</span>
-         </div>
-
-         <div className="flex flex-row">
-         <div className="text-neutral-800 text-[22px] font-medium font-['Cairo'] leading-relaxed">Age :</div>
-              <span className="text-neutral-800 text-[22px] font-light font-['Cairo'] leading-relaxed md:pl-20 pl-10 ">{age}</span>
+  };
+  return (
+    <div className="flex flex-col gap-2 mt-5">
+      <div className="flex flex-row">
+        <div className="text-neutral-800 text-[22px] font-medium font-['Cairo'] leading-relaxed">
+          Name :
         </div>
-        <button
-          type="button"
-          className="text-sm px-2 py-2 border-2 rounded-full border-primary to-secondary text-violet-950"
-          onClick={handleShowMedicalHistory}
-        >
-          Show Medical History
-        </button>
-       </div>
-    )
-}
+        <span className="text-neutral-800 text-[22px] font-light font-['Cairo'] leading-relaxed md:pl-10 pl-4">
+          {patient.patientName}
+        </span>
+      </div>
 
-export default InfoPrescription; 
+      <div className="flex flex-row">
+        <div className="text-neutral-800 text-[22px] font-medium font-['Cairo'] leading-relaxed">
+          Age :
+        </div>
+        <span className="text-neutral-800 text-[22px] font-light font-['Cairo'] leading-relaxed md:pl-20 pl-10 ">
+          {age}
+        </span>
+      </div>
+      <button
+        type="button"
+        className="text-sm px-2 py-2 border-2 rounded-full border-primary to-secondary text-violet-950"
+        onClick={handleShowMedicalHistory}
+      >
+        Show Medical History
+      </button>
+    </div>
+  );
+};
+
+export default InfoPrescription;
