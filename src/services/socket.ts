3f5ba61afc831154e9d5fadb516d1d7b61a49927
Efraim Nabil <efraimefefraim@gmail.com>
2024-06-27 15:55:53 +0300

    refactor socket connection and add authorization header

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -2,9 +2,7 @@ import { io } from "socket.io-client";
 import Cookies from 'js-cookie';
 
 const token = Cookies.get('token');
-console.log('token:', token);
 const apiUrl = "https://api.medical-society.fr.to/"
-console.log('apiUrl:', apiUrl);
 export const socket = io(apiUrl, {
     extraHeaders: {
         Authorization: `bearer ${token}`
