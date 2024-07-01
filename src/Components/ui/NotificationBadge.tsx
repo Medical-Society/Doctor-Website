752a2d185ce54a7842073dd42bb2400730dc6f43
Efraim Nabil <efraimefefraim@gmail.com>
2024-07-01 06:34:41 +0300

    Update NotificationBadge component styling

--- src/Components/ui/NotificationBadge.tsx
+++ src/Components/ui/NotificationBadge.tsx
@@ -6,10 +6,10 @@ const NotificationBadge = ({notificationCount}: IProps) => {
   return (
     <button
       type="button"
-      className="relative inline-flex items-center p-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      className="relative inline-flex items-center p-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
     >
       <svg
-        className="w-5 h-5"
+        className="w-4 h-4"
         aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
         fill="currentColor"
@@ -19,7 +19,7 @@ const NotificationBadge = ({notificationCount}: IProps) => {
         <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
       </svg>
       <span className="sr-only">Notifications</span>
-      <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -end-2 dark:border-gray-900">
+      <div className="absolute inline-flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -end-2 dark:border-gray-900">
         {notificationCount}
       </div>
     </button>
