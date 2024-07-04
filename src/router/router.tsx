81da775733587bc55ae5085fe5d65b29044b0d9b
Efraim Nabil <efraimefefraim@gmail.com>
2024-07-04 18:43:42 +0300

    make the navbar appears in login and signup page

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,8 +1,4 @@
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
+import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
@@ -12,12 +8,7 @@ import Profile from "../pages/Profile";
 import Appointments from "../pages/TodayAppointments";
 import Models from "../pages/Models";
 import ChatBox from "../Components/chats/ChatBox";
-import {
-  MainLayout,
-  ClinicLayout,
-  ChatsLayout,
-  SettingsLayout,
-} from "./layouts";
+import { MainLayout, ClinicLayout, ChatsLayout, SettingsLayout } from "./layouts";
 import Post from "../pages/Post";
 import DoctorRoom from "../pages/DoctorRoom";
 import TodayAppointments from "../pages/TodayAppointments";
@@ -27,105 +18,74 @@ import UpdatePassword from "../pages/UpdatePassword";
 import AvailableTime from "../pages/AvailableTime";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route
-          path="forget-password"
-          element={
-            <ProtectedRoute redirectPath="/" isAuth={false}>
-              <ForgetPass />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="profile"
-          element={
-            <ProtectedRoute redirectPath="/" isAuth={true}>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="post/:id"
-          element={
-            <ProtectedRoute redirectPath="/" isAuth={true}>
-              <Post />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="clinic"
-          element={
-            <ProtectedRoute redirectPath="/" isAuth={true}>
-              <ClinicLayout />
-            </ProtectedRoute>
-          }
-        >
-          <Route index element={<Appointments />} />
-          <Route path="today-appointments" element={<TodayAppointments />} />
-          <Route
-            path="finished-appointments"
-            element={<FinishedAppointments />}
-          />
-          <Route path="doctor-room" element={<DoctorRoom />} />
-        </Route>
-        <Route
-          path="chats"
-          element={
-            <ProtectedRoute redirectPath="/" isAuth={true}>
-              <ChatsLayout />
-            </ProtectedRoute>
-          }
-        >
-          <Route index element={<ChatBox />} />
-          <Route path=":id" element={<ChatBox />} />
-        </Route>
-        <Route
-          path="patient/:id/medical-history"
-          element={<MedicalHistory />}
-        />
-        <Route path="models" element={<Models />} />
-        <Route
-          path="settings"
-          element={
-            <ProtectedRoute redirectPath="/" isAuth={true}>
-              <SettingsLayout />
-            </ProtectedRoute>
-          }
-        >
-          <Route index element={<UpdatePassword />} />
-          <Route path="update-password" index element={<UpdatePassword />} />
-          <Route path="available-time" element={<AvailableTime />} />
-        </Route>
-      </Route>
-      <Route
-        path="login"
-        element={
-          <ProtectedRoute redirectPath="/" isAuth={false}>
-            <Login />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="signup"
-        element={
-          <ProtectedRoute redirectPath="/" isAuth={false}>
-            <Signup />
-          </ProtectedRoute>
-        }
-      />
+    createRoutesFromElements(
+        <>
+            <Route 
+                path="/" 
+                element={<MainLayout />}
+            >
+                <Route index element={<Home />} />
+                <Route path="login" element={
+                    <ProtectedRoute redirectPath="/" isAuth={false}>
+                        <Login />
+                    </ProtectedRoute>
+                } />
+                <Route path="signup" element={
+                    <ProtectedRoute redirectPath="/" isAuth={false}>
+                        <Signup />
+                    </ProtectedRoute>
+                } />
+                <Route path="forget-password" element={
+                    <ProtectedRoute redirectPath="/" isAuth={false}>
+                        <ForgetPass />
+                    </ProtectedRoute>
+                } />
+                <Route path="profile" element={
+                    <ProtectedRoute redirectPath="/" isAuth={true}>
+                        <Profile />
+                    </ProtectedRoute>
+                } />
+                <Route path = "post/:id" element={
+                    <ProtectedRoute redirectPath="/" isAuth={true}>
+                        <Post />
+                    </ProtectedRoute>
+                } />
+                <Route path="clinic" element={
+                    <ProtectedRoute redirectPath="/" isAuth={true}>
+                        <ClinicLayout />
+                    </ProtectedRoute>
+                }>
 
-      {/* Page Not Found */}
-      <Route
-        path="*"
-        element={
-          <h1 className="text-4xl font-bold text-center">404 Not Found</h1>
-        }
-      />
-    </>
-  )
+                    <Route index element={<Appointments />} />
+                    <Route path="today-appointments" element={<TodayAppointments />} />
+                    <Route path="finished-appointments" element={<FinishedAppointments />} />
+                    <Route path="doctor-room" element={<DoctorRoom />} />
+                </Route>
+                <Route path="chats" element={
+                    <ProtectedRoute redirectPath="/" isAuth={true}>
+                        <ChatsLayout />
+                    </ProtectedRoute>
+                }>
+                    <Route index element={<ChatBox />} />
+                    <Route path=":id" element={<ChatBox />} />
+                </Route>
+                <Route path="patient/:id/medical-history" element={<MedicalHistory />} />
+                <Route path="models" element={<Models />} />
+                <Route path="settings" element={
+                    <ProtectedRoute redirectPath="/" isAuth={true}>
+                        <SettingsLayout />
+                    </ProtectedRoute>
+                }>
+                    <Route index element={<UpdatePassword />} />
+                    <Route path="update-password" index element={<UpdatePassword />} />
+                    <Route path="available-time" element={<AvailableTime />} />
+                </Route>
+            </Route>
+
+            {/* Page Not Found */}
+            <Route path="*" element={<h1 className="text-4xl font-bold text-center">404 Not Found</h1>} />
+        </>
+    )
 );
 
 export default router;
