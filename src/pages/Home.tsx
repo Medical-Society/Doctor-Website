c55efddf78dcbbc731e3b3465c4b7e5e127d8814
Efraim Nabil <efraimefefraim@gmail.com>
2024-07-06 22:35:25 +0300

    Remove unused component from Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import AppointmentsOption from '../Components/Home/AppointmentsOption';
 import PortfolioOption from '../Components/Home/PortfolioOption';
 import ReportOption from '../Components/Home/ReportOption';
-import ReviewsMedicalOption from '../Components/Home/ReviewsMedicalOption';
+// import ReviewsMedicalOption from '../Components/Home/ReviewsMedicalOption';
 import Welcome from '../Components/Home/Welcome';
 
 
@@ -15,7 +15,7 @@ const Home = ({}: IProps) => {
   return (
     <div className="flex flex-col justify-center items-center">
       <Welcome />
-    <ReviewsMedicalOption />
+    {/* <ReviewsMedicalOption /> */}
      <ReportOption />
      <PortfolioOption />
      <AppointmentsOption /> 
